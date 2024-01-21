6682d80a47b71c41bcbe4123093d80e5c1b8eb31
DevP3PP3R <petercater@naver.com>
2024-01-21 16:43:21 +0900

    onTap WIP

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -6,11 +6,8 @@ import 'package:innerpalette/widget/bottom_nav_bar.dart';
 import 'package:innerpalette/widget/image_select.dart';
 import 'package:provider/provider.dart';
 import '../provider/color_provider.dart';
-import '../provider/img_provider.dart';
-import '../widget/color_picker.dart';
 import '../widget/main_color.dart';
 import 'pick_page.dart';
-import 'select_page.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
@@ -19,12 +16,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final imgProvider = Provider.of<ImgProvider>(context);
     final colorProvider = Provider.of<ColorProvider>(context);
     final selectedColor = colorProvider.selectedColor;
-    File? previewImage = (imgProvider.previewImage != null)
-        ? File(imgProvider.previewImage!)
-        : null;
     return SafeArea(
       child: Scaffold(
         bottomNavigationBar: const BtmNavBar(),
--- lib/widget/main_image.dart
+++ lib/widget/main_image.dart
@@ -1,6 +1,13 @@
+import 'dart:async';
 import 'dart:io';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../provider/color_provider.dart';
+import '../provider/img_provider.dart';
 
 // ignore: must_be_immutable
 class MainImage extends StatelessWidget {
@@ -15,11 +22,57 @@ class MainImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        width: deviceWidth * 0.8,
-        height: deviceWidth * 0.8,
-        decoration: BoxDecoration(
-            image:
-                DecorationImage(image: FileImage(image!), fit: BoxFit.cover)));
+    Future<ui.Image> loadImage(ImageProvider provider) async {
+      final Completer<ui.Image> completer = Completer<ui.Image>();
+      final ImageStream stream = provider.resolve(ImageConfiguration.empty);
+      final listener = ImageStreamListener(
+          (ImageInfo info, bool _) => completer.complete(info.image));
+      stream.addListener(listener);
+      return completer.future;
+    }
+
+    void onTap(TapDownDetails details) async {
+      final RenderBox box = context.findRenderObject() as RenderBox;
+      final Offset localPosition =
+          box.globalToLocal(details.globalPosition) + const Offset(10, 90);
+
+      final imgProvider = Provider.of<ImgProvider>(context, listen: true);
+      File? previewImage = imgProvider.previewImage != null
+          ? File(imgProvider.previewImage!)
+          : null;
+      final ui.Image image = await loadImage(FileImage(previewImage!));
+      final double scaleX = image.width / box.size.width;
+      final double scaleY = image.height / box.size.height;
+
+      final int x = (localPosition.dx * scaleX).round();
+      final int y = (localPosition.dy * scaleY).round();
+
+      final ByteData? byteData =
+          await image.toByteData(format: ui.ImageByteFormat.rawRgba);
+      final Uint8List uint8list = byteData!.buffer.asUint8List();
+
+      final int offset = ((y * image.width) + x) * 4;
+      final int r = uint8list[offset];
+      final int g = uint8list[offset + 1];
+      final int b = uint8list[offset + 2];
+      final int a = uint8list[offset + 3];
+
+      Color color = Color.fromARGB(a, r, g, b);
+
+      if (!context.mounted) {
+        return;
+      }
+      Provider.of<ColorProvider>(context, listen: true).setPickedColor(color);
+    }
+
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+          width: deviceWidth * 0.8,
+          height: deviceWidth * 0.8,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: FileImage(image!), fit: BoxFit.cover))),
+    );
   }
 }
